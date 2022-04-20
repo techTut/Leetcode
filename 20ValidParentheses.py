fae37283069bd5412924cae442815d3e816cc871
Bikash Mainali <mainali.bikash@icloud.com>
2022-04-20 00:03:16 -0400

    20 Solution

---
+++ 20ValidParentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {"(" : ")",
+                  "[" : "]",
+                  "{" : "}",
+                  }
+        
+        for ch in s:
+            if ch in mapping:
+                stack.append(ch)
+            else:
+                if not stack or mapping[stack.pop()] != ch:
+                    return False
+        return len(stack) == 0
+
+
+if __name__ == "__main__":
+    s = Solution()
+    for testCase in ["()", "(){}[]", "(]"]:
+        print('Output for input "{0}" is : "{1}"'.format(testCase, s.isValid(testCase)))
